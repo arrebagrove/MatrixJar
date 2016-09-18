016b2e7b624ef984457d48e079ab75626e6196b0
Artjom Graf <prizrak.nra@gmail.com>
2016-09-19 00:05:10 +0300

    Initial commit
    ++ UI;
    ++ Animations;
    ++ Navigation pane;
    ++ UserControl with resizable Matrix.

---
+++ MatrixCalc/MainPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace MatrixCalc
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            MainFrame.Navigate(typeof(Page_Plus));
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(300, 100));
+            if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.UI.ViewManagement.StatusBar"))
+            {
+                StatusBar statusBar = StatusBar.GetForCurrentView();
+                SolidColorBrush statuscolor = NavBackground.Background as SolidColorBrush;
+                statusBar.BackgroundColor = statuscolor.Color;
+                statusBar.BackgroundOpacity = 1;
+            }
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            SystemNavigationManager.GetForCurrentView().BackRequested += (s, a) =>
+            {
+                if (MainFrame.CanGoBack)
+                {
+                    MainFrame.GoBack();
+                    a.Handled = true;
+                }
+            };
+        }  
+       
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void HamburgerListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (HamburgerListBox.SelectedIndex)
+            {
+                case 1:
+                    MainFrame.Navigate(typeof(Page_Plus));
+                    break;
+                case 2:
+                    MainFrame.Navigate(typeof(Page_Minus));
+                    break;
+                case 3:
+                    MainFrame.Navigate(typeof(Page_Multi));
+                    break;
+                case 4:
+                    MainFrame.Navigate(typeof(Page_Transp));
+                    break;
+                case 5:
+                    MainFrame.Navigate(typeof(Page_Reverse));
+                    break;
+                case 6:
+                    MainFrame.Navigate(typeof(Page_Determinant));
+                    break;
+            }
+
+            MySplitView.IsPaneOpen = false;
+        }
+
+        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            HamburgerListBox.SelectedIndex = App.ChosenIndex;
+        }
+
+        private void SplitView_Open(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = true;
+        }
+
+        private void SplitView_Close(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = false;
+        }
+    }
+}
---
+++ MatrixCalc/MatrixBox.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+namespace MatrixCalc
+{
+    public sealed partial class MatrixBox : UserControl
+    {
+        public MatrixBox()
+        {
+            this.InitializeComponent();
+        }
+
+        private void AddRow_Click(object sender, RoutedEventArgs e)
+        {
+            int count = MatrixOne.RowDefinitions.Count;
+            if (count < 6)
+            {
+                RowDefinition rowDefinition = new RowDefinition();
+                MatrixOne.RowDefinitions.Add(rowDefinition);
+                for (int x = 0; x < MatrixOne.ColumnDefinitions.Count; x++)
+                {
+                    TextBox textBox = new TextBox() { Margin = new Thickness(3), MinWidth = 0 };
+                    textBox.SetValue(Grid.RowProperty, count);
+                    textBox.SetValue(Grid.ColumnProperty, x);
+                    textBox.Transitions = new TransitionCollection();
+                    textBox.Transitions.Add(new AddDeleteThemeTransition());
+                    textBox.InputScope = new InputScope()
+                    {
+                        Names = { new InputScopeName(InputScopeNameValue.Digits) }
+                    };
+                    textBox.BorderBrush = Resources["AppBarItemDisabledForegroundThemeBrush"] as Brush;
+                    MatrixOne.Children.Add(textBox);
+                }
+            }
+
+            UpdateCounter();
+        }
+
+        private async void RemRow_Click(object sender, RoutedEventArgs e)
+        {
+            int index = MatrixOne.RowDefinitions.Count - 1;
+            if (index > 1)
+            {
+                for (int x = 0; x < MatrixOne.ColumnDefinitions.Count; x++)
+                    foreach (UIElement child in MatrixOne.Children)
+                        if (((int)child.GetValue(Grid.RowProperty) == index)
+                            && ((int)child.GetValue(Grid.ColumnProperty) == x))
+                            MatrixOne.Children.Remove(child);
+                await Task.Delay(600);
+                MatrixOne.RowDefinitions.RemoveAt(index);
+            }
+
+            UpdateCounter();
+        }
+
+        private void AddColumn_Click(object sender, RoutedEventArgs e)
+        {
+            int count = MatrixOne.ColumnDefinitions.Count;
+            if (count < 6)
+            {
+                ColumnDefinition columnDefinition = new ColumnDefinition();
+                MatrixOne.ColumnDefinitions.Add(columnDefinition);
+                for (int x = 0; x < MatrixOne.RowDefinitions.Count; x++)
+                {
+                    TextBox textBox = new TextBox() { Margin = new Thickness(3), MinWidth = 0 };
+                    textBox.SetValue(Grid.RowProperty, x);
+                    textBox.SetValue(Grid.ColumnProperty, count);
+                    textBox.Transitions = new TransitionCollection();
+                    textBox.Transitions.Add(new AddDeleteThemeTransition());
+                    textBox.InputScope = new InputScope()
+                    {
+                        Names = { new InputScopeName(InputScopeNameValue.Digits) }
+                    };
+                    textBox.BorderBrush = Resources["AppBarItemDisabledForegroundThemeBrush"] as Brush;
+                    MatrixOne.Children.Add(textBox);
+                }
+            }
+
+            UpdateCounter();
+        }
+
+        private async void RemColumn_Click(object sender, RoutedEventArgs e)
+        {
+            int index = MatrixOne.ColumnDefinitions.Count - 1;
+            if (index > 1)
+            {
+                for (int x = 0; x < MatrixOne.RowDefinitions.Count; x++)
+                    foreach (UIElement child in MatrixOne.Children)
+                        if (((int)child.GetValue(Grid.ColumnProperty) == index)
+                            && ((int)child.GetValue(Grid.RowProperty) == x))
+                            MatrixOne.Children.Remove(child);
+                await Task.Delay(500);
+                MatrixOne.ColumnDefinitions.RemoveAt(index);
+            }
+
+            UpdateCounter();
+        }
+
+        private void UpdateCounter()
+        {
+            int x_num = MatrixOne.ColumnDefinitions.Count;
+            int y_num = MatrixOne.RowDefinitions.Count;
+            xC.Text = y_num.ToString();
+            yC.Text = x_num.ToString();
+        }
+    }
+}
